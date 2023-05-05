f1a0293426d596a80db74567ef30c44f5ebb8d94
Rinesa Zuzaku <119458053+RinesaZuzaku@users.noreply.github.com>
2023-05-05 17:13:47 +0200

    signup.php

---
+++ signup.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'config.php';
+
+if(isset($_POST['submit'])){
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+    
+
+
+}
+
+?>
