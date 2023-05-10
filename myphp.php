cb956dfcdc6a5be0bc3fa7483224e0049b7e2dab
Genti1bytyqi <97307142+Genti1bytyqi@users.noreply.github.com>
2023-05-10 16:40:18 +0200

    connection

---
+++ myphp.php
@@ -0,0 +1,13 @@
+<?php
+$dbhost='3306';
+$dbuser='root';
+$dbpass='';
+$conn=mysqli_connect($dbhost,$dbuser,$dbpass);
+if(! $conn )
+{
+ 
+die('Could not connect: ' . mysqli_connect_error());
+}
+
+
+?>
