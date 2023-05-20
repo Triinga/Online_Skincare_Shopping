c08881a36a10340d25be9d53ebd91cf1244ed8fb
Genti1bytyqi <97307142+Genti1bytyqi@users.noreply.github.com>
2023-05-21 00:38:11 +0200

    API

--- src/login.php
+++ src/login.php
@@ -9,6 +9,7 @@ include('../functions/common_functions.php');
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="google-signin-client_id" content="862629191035-grcft2572iiup79mgi9vj1vkeeiacfnt.apps.googleusercontent.com">
     <link rel="shortcut icon" type="image/x-icon" href="logo3.png"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -16,7 +17,7 @@ include('../functions/common_functions.php');
     <link rel="stylesheet" href="signup.php">
     <link rel="stylesheet" href="home.html">
     <script src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=f4accd94-e6da-11ed-8b76-c317f674c707"></script>
-
+    <script src="https://apis.google.com/js/platform.js" async defer> </script>
     <title>Skin care</title>
   </head>
   <body>
@@ -52,6 +53,7 @@ include('../functions/common_functions.php');
                                <button type="submit" class="btn1 mt-3 mb-5" name = "submit" name="user_login">Login</button>
                             </div>
                         </div>
+                        <div class="g-signin2" data-onsuccess="onSignIn"></div>
                         <label class="remember-me"><input type="checkbox">Remember me</label>
                         <div ><p>Don't have an account? <a href="signup.php" class="register-here"><i>Register here</i></a></p></div>
                     </form> 
@@ -61,6 +63,11 @@ include('../functions/common_functions.php');
             </div>
         </div>
      </section>
+     <script>
+        function onSignIn(googleUser){
+            console.log('User is'+JSON.stringify(googleUser.getBasicProfile()));
+        }
+     </script>
      <script src="Login.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
